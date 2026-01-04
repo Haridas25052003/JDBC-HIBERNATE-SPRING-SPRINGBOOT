70949c36fd07d0eefe8a0f9290cdd37757f05035
Haridas250503 <haridasshinde314@gmail.com>
2026-01-05 01:00:23 +0530

    intial commit

---
+++ project-01-JDBC/src/main/java/project_01_JDBC/project_01_JDBC/App.java
@@ -0,0 +1,13 @@
+package project_01_JDBC.project_01_JDBC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
