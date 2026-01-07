0f90819f1a8d415bb102908c4e4f09b485727748
Haridas250503 <haridasshinde314@gmail.com>
2026-01-07 23:48:00 +0530

    add the changes

--- project-03-JDBC-Try-Resource/src/main/java/com/demo/App.java
+++ project-03-JDBC-Try-Resource/src/main/java/com/demo/App.java
@@ -1,5 +1,7 @@
 package com.demo;
 
+import java.util.List;
+
 import com.dao.EmployeeDao;
 import com.model.Employee;
 
@@ -18,7 +20,13 @@ public class App
 //        e.setDesignation("java developer");
 //        e.setSalary(6789.0);
         
-        ed.insertEmployee(e);
+//        List<Employee> list=ed.getAllEmployees();
+//        for(Employee e1:list)
+//        	System.out.println(e1);
+        
+        e.setName("alex");
+        e.setId(1);
+        ed.updateEmployee(e);
         
         
     }
