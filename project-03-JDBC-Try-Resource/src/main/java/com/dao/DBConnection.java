b6df36871bd9d1d65c9c6d212999c2d0f76fc50a
Haridas250503 <haridasshinde314@gmail.com>
2026-01-07 13:23:46 +0530

    add the jdbc using the try resource

---
+++ project-03-JDBC-Try-Resource/src/main/java/com/dao/DBConnection.java
@@ -0,0 +1,48 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBConnection {
+	
+	public static Connection createConnection() {
+		
+		Connection con=null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/d2");
+			System.out.println("database connected successfully");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public static void closeConnection(PreparedStatement pst,Connection con) {
+		
+		try {
+			pst.close();
+			con.close();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void closeConnection(ResultSet rs,PreparedStatement pst,Connection con) {
+		
+		try {
+			rs.close();
+			pst.close();
+			con.close();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
